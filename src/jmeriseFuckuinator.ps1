4d81c7fb0fa25585323f5506e387d21962e1611c
Thomas-Toulouse-Cegep <2134063@cegepba.qc.ca>
2022-09-27 22:45:32 -0400

    rework file

---
+++ src/jmeriseFuckuinator.ps1
@@ -0,0 +1,9 @@
+﻿$Folder = "C:\Users\$env:USERNAME\Bureau\JMerise 0.5" 
+if (Test-Path -Path $Folder) {
+
+Remove-Item -Recurse $Folder | Expand-Archive C:\Users\$env:USERNAME\Bureau\JMerise.zip -DestinationPathC:\Users\$env:USERNAME\Bureau\
+    
+} else {
+	
+    Expand-Archive C:\Users\$env:USERNAME\Bureau\JMerise.zip -DestinationPath C:\Users\$env:USERNAME\Bureau\
+}
